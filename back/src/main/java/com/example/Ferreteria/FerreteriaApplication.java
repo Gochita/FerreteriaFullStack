2458d701be6cb9e57a9551bcd2080a0a09b41c7c
tamagochita <mevere66@gmail.com>
2022-03-30 15:01:13 -0500

    Estructura basica del proyecto creada

---
+++ back/src/main/java/com/example/Ferreteria/FerreteriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ferreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreteriaApplication.class, args);
+	}
+
+}
