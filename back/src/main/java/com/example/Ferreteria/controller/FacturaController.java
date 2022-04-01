9801f21451d64af3698a39f69a8db4dbcc1ce39e
tamagochita <mevere66@gmail.com>
2022-04-01 01:25:20 -0500

    Controladores para volante e inventario creados

--- back/src/main/java/com/example/Ferreteria/controller/FacturaController.java
+++ back/src/main/java/com/example/Ferreteria/controller/FacturaController.java
@@ -1,6 +1,51 @@
 package com.example.Ferreteria.controller;
 
 
+import com.example.Ferreteria.model.DTO.FacturaDTO;
+import com.example.Ferreteria.model.FacturaModel;
+import com.example.Ferreteria.services.FacturaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+@RestController
 public class FacturaController {
+    @Autowired
+    FacturaService facturaService;
+    ModelMapper mapper;
+
+
+    public FacturaController(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @PostMapping("/facturas")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<FacturaModel> save(@RequestBody FacturaDTO facturaDTO) {
+        var factura = mapper.map(facturaDTO, FacturaModel.class);
+        return this.facturaService.saveFactura(factura);
+    }
+    @GetMapping("/facturas")
+    public Flux<FacturaDTO> findAll() {
+        return this.facturaService.findAll()
+                .flatMap(f -> Mono.just(mapper.map(f, FacturaDTO.class)));
+    }
+
+    @GetMapping("/factura/{id}")
+    public Mono<FacturaDTO> findById(@PathVariable("id") String id){
+        var facturaDTOMono = this.facturaService.findById(id)
+                .flatMap(c -> Mono.just(mapper.map(c, FacturaDTO.class)));
+        return facturaDTOMono;
+    }
+
+    @DeleteMapping("/factura/{id}")
+    private Mono<ResponseEntity<FacturaModel>> delete(@PathVariable("id") String id) {
+        return this.facturaService.delete(id)
+                .flatMap(f -> Mono.just(ResponseEntity.ok(f)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
 }
--- back/src/main/java/com/example/Ferreteria/controller/VolanteController.java
+++ back/src/main/java/com/example/Ferreteria/controller/VolanteController.java
@@ -1,4 +1,53 @@
 package com.example.Ferreteria.controller;
 
+
+import com.example.Ferreteria.model.DTO.VolanteDTO;
+import com.example.Ferreteria.model.VolanteModel;
+import com.example.Ferreteria.services.VolanteService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
 public class VolanteController {
+
+    @Autowired
+    VolanteService volanteService;
+    ModelMapper mapper;
+
+    public VolanteController(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @PostMapping("/volantes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<VolanteModel> save(@RequestBody VolanteDTO volanteDTO) {
+        var volante = mapper.map(volanteDTO, VolanteModel.class);
+        return this.volanteService.save(volante);
+    }
+
+    @GetMapping("/volantes")
+    public Flux<VolanteDTO> findAll() {
+        return this.volanteService.findAll()
+                .flatMap(v -> Mono.just(mapper.map(v, VolanteDTO.class)));
+    }
+
+    @GetMapping("/volante/{id}")
+    public Mono<VolanteDTO> findById(@PathVariable("id") String id){
+        var volante = this.volanteService.findById(id)
+                .flatMap(v -> Mono.just(mapper.map(v, VolanteDTO.class)));
+        return volante;
+    }
+
+    @DeleteMapping("/volante/{id}")
+    private Mono<ResponseEntity<VolanteModel>> delete(@PathVariable("id") String id) {
+        return this.volanteService.delete(id)
+                .flatMap(c -> Mono.just(ResponseEntity.ok(c)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
 }
